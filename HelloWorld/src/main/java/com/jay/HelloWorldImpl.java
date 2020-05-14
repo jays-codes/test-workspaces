c00376b3b982a66aa76ea850e3f95487ed8de098
jay menorca <zai_menorca@yahoo.com>
2020-05-14 09:58:16 -0400

    14may; added mvn project HelloWorld

---
+++ HelloWorld/src/main/java/com/jay/HelloWorldImpl.java
@@ -0,0 +1,10 @@
+package com.jay;
+
+public class HelloWorldImpl {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World 5 !!!!");
+
+	}
+
+}
--- test_proj_java3/src/com/jay/test3/TestClass3.java
+++ test_proj_java3/src/com/jay/test3/TestClass3.java
@@ -3,7 +3,7 @@ package com.jay.test3;
 public class TestClass3 {
 
 	public static void main(String[] args) {
-		System.out.print("Java, Eclipse and GIT/GH, awesome. testing #3");
+		System.out.print("Java, Eclipse and GIT/GH, awesome. testing #4");
 
 	}
 
