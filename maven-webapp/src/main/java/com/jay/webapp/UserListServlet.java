c67d8fa264743ab865b8bcadfdc39f67198a6858
jay menorca <zai_menorca@yahoo.com>
2020-06-21 18:42:15 -0400

    commit mvn webapp war

---
+++ maven-webapp/src/main/java/com/jay/webapp/UserListServlet.java
@@ -0,0 +1,55 @@
+package com.jay.webapp;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.jay.model.User;
+
+/**
+ * Servlet implementation class UserListServlet
+ */
+@WebServlet("/user-list")
+public class UserListServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UserListServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		User mary = new User("Mary", 119);
+		User james = new User("James Junior", 118);
+		User pat = new User("Pat", 122);
+		User john = new User("John", 123);
+		
+		List<User> users = Arrays.asList(mary, james, pat, john);
+		request.setAttribute("userList", users);
+		request.getRequestDispatcher("users-list.jsp").forward(request, response);
+		
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
